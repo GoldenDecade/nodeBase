ab9d30a62061aa626f716161c65948dd7c6599d2
wangqiang <wangqiangb@litsoft.com.cn>
2019-02-24 21:51:50 +0800

    静态资源加载； notice： 1. 路径是否使用path 模块 2. ejs文件中 可以省略 /public; 直接写成 href="css/main.js"
    Signed-off-by: wangqiang <wangqiangb@litsoft.com.cn>

--- blog/app.js
+++ blog/app.js
@@ -1,15 +1,23 @@
 // 应用启动入口文件
-const express = require('express')
-const ejs = require('ejs')
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 
 //创建APP应用 ==》 http.createServer()
 const app = express();
 
+//设置静态文件托管
+//当用户访问的url以 /public开始，那么直接返回对应__dirname + '/public' 下的文件
+// app.use('/public', express.static(path.join(__dirname + '/public'))); // 使用或者不使用 path 模块都行，其实就是路径写对就好了
+//==引入文件时候，是否省略 /public ?==========
+// app.use('/public', express.static(__dirname + '/public')); // link  href="/public/css/main.css"
+app.use(express.static(__dirname + '/public'));// 可以直接使用 link  href="css/main.css"
+
 //==使用 ejs 模板 步骤： 1.设置目录 2. 注册模板引擎==================
 //1. 设置模板存放目录
-app.set('views', __dirname + '/views') // 第一个参数必须是views（不可变）， 第二个参数是目录名
+app.set('views', __dirname + '/views'); // 第一个参数必须是views（不可变）， 第二个参数是目录名
 //2. 注册模板引擎
-app.set('view engine', 'ejs') // 第一个参数必须是 view engine； 第二个参数必须和 app.engine 中的第一个参数一致，表示用什么引擎解析模板
+app.set('view engine', 'ejs'); // 第一个参数必须是 view engine； 第二个参数必须和 app.engine 中的第一个参数一致，表示用什么引擎解析模板
 
 
 
@@ -30,7 +38,7 @@ app.get('/', (req, res, next)=> {
     content: 'hello'
   })
 
-})
+});
 
 
 //监听app请求
