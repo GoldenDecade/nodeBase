24015fcfcf81a42167370eb5f68f274a36f89d4e
wangqiang <wangqiangb@litsoft.com.cn>
2019-02-24 15:44:18 +0800

    启动项目

--- blog/app.js
+++ blog/app.js
@@ -1,1 +1,17 @@
 // 应用启动入口文件
+const express = require('express')
+
+
+//创建APP应用 ==》 http.createServer()
+const app = express();
+
+/*
+* get
+* */
+app.get('/', (req, res, next)=> {
+  res.send('<h1>我的博客！</h1>')
+})
+
+
+//监听app请求
+app.listen(8081);
